2f4891581db7db9affac829844fb272121ebf3a2
youngman <sksn12@naver.com>
2026-07-01 15:04:32 +0900

    fix: 오라클 DB 소문자 컬럼 매핑을 위한 xml 매퍼 내 큰따옴표 랩핑 적용

--- src/test/java/com/example/smartmessaging/security/UserInsertTest.java
+++ src/test/java/com/example/smartmessaging/security/UserInsertTest.java
@@ -18,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 public class UserInsertTest {
 
-
     @Autowired
     private UserMapper userMapper;
 
@@ -87,3 +86,5 @@ public class UserInsertTest {
         System.out.println("아이디(사원번호): 1001 | 비밀번호: 0000");
     }
 }
+
+
