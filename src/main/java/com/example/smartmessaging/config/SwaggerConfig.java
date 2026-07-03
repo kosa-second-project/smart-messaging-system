18f2f6b73e02f8fd81db3c5f564671a0b9d8a84a
0-x-14 <0_x_14@naver.com>
2026-07-03 11:40:39 +0900

    docs: Swagger API 문서화 설정 추가

---
+++ src/main/java/com/example/smartmessaging/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.example.smartmessaging.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI messagingAPI() {
+        Info info = new Info()
+                .title("Smart Messaging System API")
+                .description("Smart Messaging System API 명세서")
+                .version("1.0.0");
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info);
+    }
+}
